49e8ba6a413a44adc04a8adf21c4e4ac8f92bfe1
ramazansakin <ramazansakin63@gmail.com>
2024-06-08 07:21:19 +0300

    feat: project base initialized

---
+++ src/main/java/com/rsakin/csv_manager/CsvManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rsakin.csv_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsakin/csv_manager/CsvManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsakin.csv_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
